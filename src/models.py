7fdc74c98c48ba30b0200eee5a1924ee9b58be47
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-05-03 04:52:49 +0000

    Modelo de base de datos starswar completo

--- src/models.py
+++ src/models.py
@@ -7,23 +7,61 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String)
+    password = Column(String)
+    email = Column(String)
+    name = Column(String)
+    favourite_planets = relationship("FavouritePlanet", back_populates="user")
+    favourite_characters = relationship("FavouriteCharacter", back_populates="user")
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    image = Column(String)
+    characters = relationship("Character", back_populates="planet")
+
+class Character(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    image = Column(String)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship("Planet", back_populates="characters")
+    ships = relationship("Ship", back_populates="character")
+
+class Ship(Base):
+    __tablename__ = 'ships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    image = Column(String)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship("Character", back_populates="ships")
+
+class FavouritePlanet(Base):
+    __tablename__ = 'favourite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user = relationship("User", back_populates="favourite_planets")
+    planet = relationship("Planet")
+
+class FavouriteCharacter(Base):
+    __tablename__ = 'favourite_character'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    user = relationship("User", back_populates="favourite_characters")
+    character = relationship("Character")
+
 
     def to_dict(self):
         return {}
